88fa80df96a07550617566dc35c73cc489fe1139
kongtb <13103820893@163.com>
2022-01-20 23:39:42 +0800

    etag

--- koaServerForHtml/index.js
+++ koaServerForHtml/index.js
@@ -3,37 +3,54 @@ import staticServer from 'koa-static';
 import koaMount from 'koa-mount';
 import KoaRouter from '@koa/router';
 
-import { resolvePath, isHtml, isCss, isJs, LimtTimeAddNow } from './util/index.js';
-
+import { resolvePath, isHtml, isCss, isJs, LimtTimeAddNow, getFileLastEditDate, setEtag } from './util/index.js';
 
 const app = new Koa();
 const router = new KoaRouter();
 const serverPort = 3001;
 
+function resolveFileUrl(url) {
+  if (url) {
+    const reqUrlArr = url.split('/');
+    reqUrlArr.splice(0, 1);
+    return resolvePath('htmlFiles', ...reqUrlArr);
+  }
+  return '';
+}
+
 app.use(async (ctx, next) => {
   await next();
   if (isHtml(ctx.url)) {
-    ctx.response.set('Cache-Control', ['max-age=3600', 'public'])
-    ctx.response.set('Expires', LimtTimeAddNow(60*60*1000))
+    // ctx.response.set('Cache-Control', 'max-age=3600, public')
+    // ctx.response.set('Expires', LimtTimeAddNow(60*60*1000))
+    // ctx.response.set('Cache-Control', 'no-cache');
+    // ctx.response.set('Cache-Control', 'no-store');
+    ctx.response.set('Cache-Control', 'no-cache, public');
+    const fileLastChangeDateUtc = await getFileLastEditDate(resolveFileUrl(ctx.url));
+    // 设置协商缓存--Last-Modified
+    ctx.response.set('Last-Modified', fileLastChangeDateUtc);
+    setEtag(ctx);
   }
-  if (isJs(ctx.url)) {
-    console.log('js')
-  }
-  if(isCss(ctx.url)){
-    console.log('css')
-  }
-})
+  // if (isJs(ctx.url)) {
+  //   console.log('js')
+  // }
+  // if(isCss(ctx.url)){
+  //   console.log('css')
+  // }
+});
 
-app.use(staticServer(resolvePath('htmlFiles'), {
-  index: 'index.html'
-}))
+app.use(
+  staticServer(resolvePath('htmlFiles'), {
+    index: 'index.html',
+  })
+);
 
 app.use(async (ctx) => {
   if (!ctx.body) {
     ctx.body = '404 NotFound';
   }
-})
+});
 
 app.listen(serverPort, () => {
   console.log(`server is runing in http://localhost:${serverPort}`);
-})
+});
--- koaServerForHtml/util/index.js
+++ koaServerForHtml/util/index.js
@@ -1,24 +1,60 @@
 import { fileURLToPath } from 'url';
 import path, { dirname as pathDirname } from 'path';
+import fs from 'fs/promises';
+import { createHash } from 'crypto';
 
 export const filename = fileURLToPath(import.meta.url);
 export const dirname = pathDirname(filename);
 
-export function resolvePath (...args) {
-  const root = path.resolve(dirname, '../', ...args)
+export function resolvePath(...args) {
+  const root = path.resolve(dirname, '../', ...args);
   return root;
 }
 
-export function isHtml (str) {
+export function isHtml(str) {
   return /\.html$/.test(str);
 }
-export function isCss (str) {
+export function isCss(str) {
   return /\.css$/.test(str);
 }
-export function isJs (str) {
+export function isJs(str) {
   return /\.js$/.test(str);
 }
 
 export function LimtTimeAddNow(num) {
   return new Date(new Date().getTime() + num).toUTCString();
+}
+
+export async function getFileLastEditDate(fileUrl) {
+  try {
+    const stats = await fs.stat(fileUrl);
+    return stats.ctime.toUTCString();
+  } catch (err) {
+    return '';
+  }
+}
+
+export async function setEtag(ctx) {
+  const url = ctx.url;
+  const urlArr = url.split('/');
+  urlArr.splice(0, 1);
+  const fileUrl = resolvePath('htmlFiles', ...urlArr);
+  const reqIfNoneMatch = ctx.request.header['if-None-Match'];
+  console.log(reqIfNoneMatch);
+  try {
+    await fs.readFile(fileUrl, 'utf-8').then((res) => {
+      const md5 = createHash('md5');
+      md5.update(res);
+      const etag = md5.digest('hex');
+      if (reqIfNoneMatch && reqIfNoneMatch === etag) {
+        ctx.response.status = 304;
+      } else {
+        // 发送新资源
+        console.log(etag);
+        ctx.response.etag = etag;
+      }
+    });
+  } catch (err) {
+    console.log('set etag error');
+  }
 }
